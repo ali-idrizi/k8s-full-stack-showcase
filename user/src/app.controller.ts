04084cb347c5dd3c0dc5256b595b5ce256562981
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-04 17:01:02 +0200

    chore(*): setup nats communication between auth and user

--- user/src/app.controller.ts
+++ user/src/app.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { BadRequestException, Body, Controller, Get, Inject, Post } from '@nestjs/common'
+import { ClientProxy } from '@nestjs/microservices'
 import { User } from '@prisma/client'
+import { catchError, Observable } from 'rxjs'
+import { ITokenPair } from './app.interface'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTH_SERVICE') private client: ClientProxy,
+  ) {}
 
   @Get('/')
   async getUsers(): Promise<User[]> {
@@ -14,5 +20,23 @@ export class AppController {
   @Post('/add')
   async addUser(@Body('name') name: string): Promise<User> {
     return await this.appService.addUser(name)
+  }
+
+  @Post('/login')
+  login(): Observable<ITokenPair> {
+    return this.client
+      .send<ITokenPair>(
+        {
+          cmd: 'generateTokenPair',
+        },
+        {
+          userId: 'test',
+        },
+      )
+      .pipe(
+        catchError((err) => {
+          throw new BadRequestException(err.message)
+        }),
+      )
   }
 }
