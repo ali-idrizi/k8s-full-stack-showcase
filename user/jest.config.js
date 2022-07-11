7b26096e689c276e7236933f8fe85d9e6f1cfa30
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-11 13:04:01 +0200

    chore(user): move jest config to jest.config.js and specify roots

---
+++ user/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rootDir: './',
+  roots: ['./src', './test/unit'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+}
