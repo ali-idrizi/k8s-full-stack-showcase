29f8063edf506c8143af1fc8dd4600f2c79f7692
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-11 19:15:09 +0200

    chore(user): add min length to register password

--- user/src/register/register.dto.ts
+++ user/src/register/register.dto.ts
@@ -1,15 +1,20 @@
 import { Type } from 'class-transformer'
-import { IsDefined } from 'class-validator'
+import { IsDefined, MinLength } from 'class-validator'
 import { Match } from 'src/common/decorators/match.decorator'
 import { LoginDto } from 'src/login/login.dto'
 
 export class RegisterDto extends LoginDto {
+  @MinLength(8, {
+    message: 'Password must be at least 8 characters',
+  })
+  override password: string
+
   @IsDefined({
     message: 'Confirm Password is required',
   })
-  @Type(() => String)
   @Match('password', {
     message: 'Confirm Password does not match',
   })
+  @Type(() => String)
   confirmPassword: string
 }
