ac4993b06b808854c1726d3763733bba95a87049
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-11 17:18:49 +0200

    chore(user): improve HashUtil test

--- user/test/unit/hash.util.spec.ts
+++ user/test/unit/hash.util.spec.ts
@@ -1,19 +1,20 @@
 import { HashUtil } from 'src/common/utils/hash.util'
 
 describe('HashUtil', () => {
-  let hash: string
-
-  describe('hash', () => {
-    beforeAll(async () => {
-      hash = await HashUtil.hash('password')
-    })
-
+  describe(HashUtil.hash, () => {
     it('should return a long hash', async () => {
+      const hash = await HashUtil.hash('password')
       expect(hash.length).toBeGreaterThan(20)
     })
   })
 
-  describe('isValid', () => {
+  describe(HashUtil.isValid, () => {
+    let hash: string
+
+    beforeAll(async () => {
+      hash = await HashUtil.hash('password')
+    })
+
     it('should return true if password is correct', async () => {
       const isPasswordValid = await HashUtil.isValid('password', hash)
       expect(isPasswordValid).toBe(true)
