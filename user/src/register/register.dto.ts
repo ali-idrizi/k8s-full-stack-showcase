b571928a5b74215210b2742f52e938846483a88f
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-12 10:36:48 +0200

    chore(user): add name field in register dto

--- user/src/register/register.dto.ts
+++ user/src/register/register.dto.ts
@@ -4,6 +4,15 @@ import { Match } from 'src/common/decorators/match.decorator'
 import { AuthInputDto } from 'src/common/dto/auth-input.dto'
 
 export class RegisterDto extends AuthInputDto {
+  @IsDefined({
+    message: 'Name is required',
+  })
+  @MinLength(3, {
+    message: 'Name must be at least 3 characters',
+  })
+  @Type(() => String)
+  name: string
+
   @MinLength(8, {
     message: 'Password must be at least 8 characters',
   })
