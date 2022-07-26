d87676675f602bba2dd95e835ed2fdd54197fb41
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-26 14:35:39 +0200

    feat(*): add helm charts

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
-const NATS_HOST = process.env.NATS_SERVICE_SERVICE_HOST;
-const NATS_PORT = process.env.NATS_SERVICE_SERVICE_PORT;
+const NATS_HOST = process.env.NATS_SERVICE_HOST;
+const NATS_PORT = process.env.NATS_SERVICE_PORT;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
