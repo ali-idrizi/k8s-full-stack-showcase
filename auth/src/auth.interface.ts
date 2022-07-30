80b301789b5d47af7e2f63bb39e6512ce4c5c572
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-07-30 15:24:18 +0200

    chore(auth): remove unused interface

--- auth/src/auth.interface.ts
+++ auth/src/auth.interface.ts
@@ -3,10 +3,6 @@ export interface ITokenPair {
   readonly refreshToken: string
 }
 
-export interface IGenerateTokenPairBody {
-  readonly userId: string
-}
-
 export interface IEnvironment {
   readonly JWT_EXPIRES_IN_SECONDS: number
   readonly JWT_SECRET: string
