c6d0609f70a791fdee26d208fbcb858d2b7ec0d1
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-08-02 18:30:54 +0200

    docs(auth): add JSDoc for error util isNotFoundError

--- auth/src/common/utils/error.util.ts
+++ auth/src/common/utils/error.util.ts
@@ -1,6 +1,13 @@
 import { Prisma } from '@prisma/client'
 
 export const ErrorUtil = {
+  /**
+   * Retuns true if the error is a Prisma not found error
+   *
+   * @param error the error that was thrown
+   *
+   * @returns true if the error is a Prisma not found error (P2025)
+   */
   isNotFoundError: (error: unknown): boolean => {
     return error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025'
   },
