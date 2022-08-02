2fe91b39a44b986a5d68f254408ac8443bcc2cd6
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-08-02 14:45:09 +0200

    refactor(auth): create constants for MessagePattern commands

--- auth/src/auth.constant.ts
+++ auth/src/auth.constant.ts
@@ -4,3 +4,10 @@ export const Environment = {
   NATS_HOST: 'NATS_HOST',
   NATS_PORT: 'NATS_PORT',
 } as const
+
+export const Command = {
+  GENERATE_TOKEN_PAIR: 'generateTokenPair',
+  VALIDATE_JWT: 'validateJwt',
+  REFRESH_JWT: 'refreshJwt',
+  REMOVE_REFRESH_TOKEN: 'removeRefreshToken',
+} as const
--- auth/src/auth.controller.ts
+++ auth/src/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common'
 import { MessagePattern, Payload } from '@nestjs/microservices'
-import { TokenPair, RemoveRefreshTokenRes, ValidateJwtRes } from './auth.interface'
+import { Command } from './auth.constant'
+import { RemoveRefreshTokenRes, TokenPair, ValidateJwtRes } from './auth.interface'
 import { AuthService } from './auth.service'
 import { GenerateTokenPairDto } from './dto/generate-token-pair.dto'
 import { RefreshJwtDto } from './dto/refresh-jwt.dto'
@@ -11,23 +12,23 @@ import { ValidateJwtDto } from './dto/validate-jwt.dto'
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @MessagePattern({ cmd: 'generateTokenPair' })
+  @MessagePattern({ cmd: Command.GENERATE_TOKEN_PAIR })
   generateTokenPair(@Payload() payload: GenerateTokenPairDto): Promise<TokenPair> {
     return this.authService.generateTokenPair(payload)
   }
 
-  @MessagePattern({ cmd: 'validateJwt' })
+  @MessagePattern({ cmd: Command.VALIDATE_JWT })
   validateJwt(@Payload() payload: ValidateJwtDto): ValidateJwtRes {
     return this.authService.validateJwt(payload)
   }
 
-  @MessagePattern({ cmd: 'refreshJwt' })
+  @MessagePattern({ cmd: Command.REFRESH_JWT })
   async refreshJwt(@Payload() payload: RefreshJwtDto): Promise<TokenPair> {
     await this.authService.removeRefreshToken(payload.refreshToken)
     return this.authService.generateTokenPair({ userId: payload.userId })
   }
 
-  @MessagePattern({ cmd: 'removeRefreshToken' })
+  @MessagePattern({ cmd: Command.REMOVE_REFRESH_TOKEN })
   removeRefreshToken(@Payload() payload: RemoveRefreshTokenDto): Promise<RemoveRefreshTokenRes> {
     return this.authService.removeRefreshToken(payload.refreshToken)
   }
