b6fffd31d4ca7a766054cd775acc7c3b40fdd444
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-08-17 17:18:16 +0200

    chore(todo): remove unneeded prisma import

--- todo/src/item/item.module.ts
+++ todo/src/item/item.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
-import { PrismaModule } from 'nestjs-prisma'
 import { ItemController } from './item.controller'
 import { ItemService } from './item.service'
 
 @Module({
-  imports: [PrismaModule.forRoot()],
   controllers: [ItemController],
   providers: [ItemService],
 })
