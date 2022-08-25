f3d8236bf5f903ed18dad79378cc8626cac83eb8
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-08-25 13:21:44 +0200

    refactor(user): add new lines to UserId decorator

--- user/src/common/decorators/user-id.decorator.ts
+++ user/src/common/decorators/user-id.decorator.ts
@@ -7,8 +7,10 @@ import { ReqUtil } from '../utils/req.util'
 
 export const UserId = createParamDecorator((_: never, ctx: ExecutionContext): string => {
   const userId = ReqUtil.getHeaderFromContext(ctx, 'X-User-ID')
+
   if (!userId) {
     throw new InternalServerErrorException()
   }
+
   return userId
 })
