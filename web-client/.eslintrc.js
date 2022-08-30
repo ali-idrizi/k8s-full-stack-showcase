b3f39d1d3b22a8e86f29288c3580053bfa5910e3
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-08-30 16:18:35 +0200

    chore(web-client): init nextjs app with jest

---
+++ web-client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['testing-library', '@typescript-eslint/eslint-plugin'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/explicit-module-boundary-types': ['error'],
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
+}
---
+++ web-client/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '@/styles/global.css'
+import type { AppProps } from 'next/app'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
