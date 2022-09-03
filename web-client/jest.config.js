48a1270f30e165f079339ac0e2052686200a043a
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-03 16:59:30 +0200

    chore(web-client): update configs to always treat @/* import as src/*

--- web-client/jest.config.js
+++ web-client/jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '^@/(.*)/(.*)$': '<rootDir>/src/$1/$2',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }
