b6fcc0c5f32a6f803fe8364526527db7fabbfcb7
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-12 19:02:25 +0200

    chore(web-client): add expect that react query called the data function once

--- web-client/__tests__/pages/index.spec.tsx
+++ web-client/__tests__/pages/index.spec.tsx
@@ -2,18 +2,13 @@ import Home from '@/pages/index'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { render, screen } from '@testing-library/react'
 
+const getMockData = jest.fn().mockResolvedValue([{ title: 'Test' }])
+
 describe('Home', () => {
   const queryClient = new QueryClient()
 
   beforeEach(async () => {
-    await queryClient.fetchQuery(
-      ['todos'],
-      jest.fn().mockResolvedValue([
-        {
-          title: 'Test',
-        },
-      ]),
-    )
+    await queryClient.fetchQuery(['todos'], getMockData)
   })
 
   it('renders a heading', async () => {
@@ -29,6 +24,7 @@ describe('Home', () => {
 
     const todos = await screen.findByTestId('todos')
 
+    expect(getMockData).toHaveBeenCalledTimes(1)
     expect(heading).toBeInTheDocument()
     expect(todos).toHaveTextContent('Test')
   })
