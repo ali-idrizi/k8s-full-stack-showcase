a259030197f35aa0fb2f1a2b2ff2f7bd1d3f8236
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-12 18:30:53 +0200

    chore(web-client): add test for withReactQuery hoc

---
+++ web-client/__tests__/hocs/withReactQuery.spec.ts
@@ -0,0 +1,75 @@
+import { withReactQuery } from '@/hocs'
+import { createMockContext, MockContext } from '@/utils/test/mockContext'
+
+const getMockData = jest.fn().mockResolvedValue({
+  foo: 'bar',
+})
+
+describe('With React Query GSSP HOC', () => {
+  let ctx: MockContext
+
+  beforeEach(async () => {
+    ctx = createMockContext()
+  })
+
+  it('should correctly dehydrate the state', async () => {
+    const gssp = withReactQuery(async ({ queryClient }) => {
+      await queryClient.prefetchQuery(['todos'], getMockData)
+
+      return {
+        props: {},
+      }
+    })
+
+    const res = await gssp(ctx.context)
+
+    if (!('props' in res)) {
+      throw new Error('`props` excepted to be returned from GSSP')
+    }
+
+    const props = await Promise.resolve(res.props)
+
+    expect(props.dehydratedState).toBeTruthy()
+    expect(props.dehydratedState.queries.at(0)?.state.data).toEqual({ foo: 'bar' })
+  })
+
+  it('should retain original props', async () => {
+    const gssp = withReactQuery(() => {
+      return {
+        props: {
+          foo: 'bar',
+        },
+      }
+    })
+
+    const res = await gssp(ctx.context)
+
+    if (!('props' in res)) {
+      throw new Error('`props` excepted to be returned from GSSP')
+    }
+
+    const props = await Promise.resolve(res.props)
+    expect(props.foo).toBe('bar')
+  })
+
+  it('should return notFound and redirect from GSSP', async () => {
+    let gssp = withReactQuery(() => {
+      return {
+        notFound: true,
+      }
+    })
+    let res = await gssp(ctx.context)
+    expect('notFound' in res).toBe(true)
+
+    gssp = withReactQuery(() => {
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      }
+    })
+    res = await gssp(ctx.context)
+    expect('redirect' in res).toBe(true)
+  })
+})
