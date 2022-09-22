fb9a68789bb94997b23c88da6b1b80425bdd8c75
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-22 21:58:36 +0200

    chore(web-client): add / link to logo

--- web-client/src/components/header/logo.tsx
+++ web-client/src/components/header/logo.tsx
@@ -1,6 +1,5 @@
-import { useColorModeValue } from '@chakra-ui/color-mode'
-import { Icon } from '@chakra-ui/icon'
-import { HStack, Text } from '@chakra-ui/layout'
+import { Icon, Link, Text, useColorModeValue } from '@chakra-ui/react'
+import NextLink from 'next/link'
 import { FiCheckCircle } from 'react-icons/fi'
 
 export const Logo: React.FC = () => {
@@ -8,18 +7,27 @@ export const Logo: React.FC = () => {
   const colorTo = useColorModeValue('blue.600', 'blue.200')
 
   return (
-    <HStack data-testid="logo" userSelect="none" spacing={2}>
-      <Icon as={FiCheckCircle} w={[5, null, 7]} h={[5, null, 7]} color={colorFrom} />
-      <Text
-        as="span"
-        lineHeight="1"
-        bgGradient={`linear(to-r, ${colorFrom}, ${colorTo})`}
-        bgClip="text"
-        fontSize={['lg', null, 'xl']}
-        fontWeight="extrabold"
+    <NextLink href="/" passHref>
+      <Link
+        data-testid="logo"
+        _hover={{ textDecoration: 'none' }}
+        display="flex"
+        gap={2}
+        userSelect="none"
+        alignItems="center"
       >
-        TODO
-      </Text>
-    </HStack>
+        <Icon as={FiCheckCircle} w={[5, null, 7]} h={[5, null, 7]} color={colorFrom} />
+        <Text
+          as="span"
+          lineHeight="1"
+          bgGradient={`linear(to-r, ${colorFrom}, ${colorTo})`}
+          bgClip="text"
+          fontSize={['lg', null, 'xl']}
+          fontWeight="extrabold"
+        >
+          TODO
+        </Text>
+      </Link>
+    </NextLink>
   )
 }
