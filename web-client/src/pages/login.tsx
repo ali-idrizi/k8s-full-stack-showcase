2845ccc883ed63072efe5790b8e7da7acd9cd90a
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-24 19:16:08 +0200

    chore(web-client): extract auth layout from login page

--- web-client/src/pages/login.tsx
+++ web-client/src/pages/login.tsx
@@ -1,20 +1,14 @@
-import { Button, Container, GradientHeading, LabelInput } from '@/components'
+import { Button, LabelInput } from '@/components'
 import { useBrandColors } from '@/hooks'
 import { AuthLayout } from '@/layouts'
 import { PageWithLayout } from '@/utils/types'
-import {
-  Box,
-  FormControl,
-  Heading,
-  Highlight,
-  Text,
-  useColorModeValue,
-  VStack,
-} from '@chakra-ui/react'
+import { FormControl, Heading, VStack } from '@chakra-ui/react'
 import { useFormik } from 'formik'
 import { FiAtSign, FiKey } from 'react-icons/fi'
 
 const Login: PageWithLayout = () => {
+  const { primary, primaryScheme } = useBrandColors()
+
   const formik = useFormik({
     initialValues: {
       email: '',
@@ -25,93 +19,43 @@ const Login: PageWithLayout = () => {
     },
   })
 
-  const { primary, primaryScheme } = useBrandColors()
-
   return (
-    <Box
-      bgGradient={{
-        lg: `linear(to-l, ${useColorModeValue('gray.50', 'whiteAlpha.50')} 50%, transparent 0%)`,
-      }}
-      display="flex"
-      minH="full"
-    >
-      <Container flexDir={{ base: 'column', lg: 'row' }} justifyContent={{ lg: 'space-between' }}>
-        <Box flexGrow={{ lg: 1 }} bg={useColorModeValue('white', 'gray.800')}>
-          <GradientHeading
-            as="h2"
-            lineHeight="tall"
-            mt={{ base: 10, lg: 16 }}
-            fontSize={['2xl', '3xl', null, '4xl', '5xl']}
-            textAlign={{ base: 'center', lg: 'left' }}
-          >
-            Create and manage your
-            <br />
-            <Text as="span" whiteSpace="nowrap">
-              <Highlight
-                query={['tasks', 'todos']}
-                styles={{
-                  px: '4',
-                  py: '1',
-                  bg: 'orange.100',
-                  rounded: 'full',
-                  transition: 'background .3s',
-                  _hover: { bg: 'red.100' },
-                }}
-              >
-                tasks and todos
-              </Highlight>
-            </Text>
-          </GradientHeading>
-        </Box>
-        <Box
-          minW="28rem"
-          borderStyle="solid"
-          borderColor={useColorModeValue('gray.100', 'gray.700')}
-          borderLeftWidth={{ lg: 10 }}
-          flexGrow={{ base: 1, lg: 0 }}
-          pl={{ base: 8, lg: 12 }}
-          pr={{ base: 8, lg: 0 }}
-          mt={{ base: 10, lg: 0 }}
-          mx={{ base: -4, lg: 0 }}
-          bg={{ base: useColorModeValue('gray.50', 'whiteAlpha.50'), lg: 'none' }}
-        >
-          <Heading as="h1" mt={{ base: 10, lg: 16 }} mb={8} color={primary} lineHeight="tall">
+    <>
+      <Heading as="h1" mb={8} color={primary} lineHeight="tall">
+        Login
+      </Heading>
+
+      <form onSubmit={formik.handleSubmit}>
+        <VStack spacing={8} align="flex-start">
+          <FormControl>
+            <LabelInput
+              id="email"
+              label="Email Address"
+              type="text"
+              autoFocus
+              iconAs={FiAtSign}
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+          </FormControl>
+
+          <FormControl>
+            <LabelInput
+              id="password"
+              label="Password"
+              type="password"
+              iconAs={FiKey}
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+          </FormControl>
+
+          <Button type="submit" colorScheme={primaryScheme} width="full">
             Login
-          </Heading>
-
-          <form onSubmit={formik.handleSubmit}>
-            <VStack spacing={8} align="flex-start">
-              <FormControl>
-                <LabelInput
-                  id="email"
-                  label="Email Address"
-                  type="text"
-                  autoFocus
-                  iconAs={FiAtSign}
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                />
-              </FormControl>
-
-              <FormControl>
-                <LabelInput
-                  id="password"
-                  label="Password"
-                  type="password"
-                  iconAs={FiKey}
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                />
-              </FormControl>
-
-              <Button type="submit" colorScheme={primaryScheme} width="full">
-                Login
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </Container>
-    </Box>
+          </Button>
+        </VStack>
+      </form>
+    </>
   )
 }
 
