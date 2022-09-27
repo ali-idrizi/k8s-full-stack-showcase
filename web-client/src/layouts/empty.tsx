35542a21b0f7ee2dce1f667789639a24766febef
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-27 16:32:30 +0200

    chore(web-client): add py to empty layout

--- web-client/src/layouts/empty.tsx
+++ web-client/src/layouts/empty.tsx
@@ -1,5 +1,9 @@
 import { Container } from '@/components'
 
 export const EmptyLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <Container flexDir="column">{children}</Container>
+  return (
+    <Container flexDir="column" py="4">
+      {children}
+    </Container>
+  )
 }
