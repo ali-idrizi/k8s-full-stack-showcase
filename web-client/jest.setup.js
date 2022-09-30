83054e3a179453d28fc7a69b4dc1cb8576b0fe80
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-09-30 22:18:26 +0200

    refactor(web-client): remove jest setup file

--- web-client/jest.setup.js
+++ web-client/jest.setup.js
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom/extend-expect'
+
+// https://stackoverflow.com/a/71817340/1354378
+jest.mock('next/dynamic', () => ({
+  __esModule: true,
+  default: (...props) => {
+    const dynamicModule = jest.requireActual('next/dynamic')
+    const dynamicActualComp = dynamicModule.default
+    const RequiredComponent = dynamicActualComp(props[0])
+    RequiredComponent.preload ? RequiredComponent.preload() : RequiredComponent.render.preload()
+    return RequiredComponent
+  },
+}))
