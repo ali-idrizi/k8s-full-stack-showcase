7fd2a11b304f647583bc9f7a9c798026bedd1ff7
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-06 16:01:39 +0200

    fix(web-client): todo-list query api call

--- web-client/src/hooks/queries/todo-list.ts
+++ web-client/src/hooks/queries/todo-list.ts
@@ -3,9 +3,9 @@ import { TodoList } from '@/utils/types'
 import { useQuery, UseQueryResult } from '@tanstack/react-query'
 
 export const useTodoLists = (): UseQueryResult<TodoList[], ApiError> => {
-  return useQuery(['todo', 'lists'], API().todo.list.getAll)
+  return useQuery(['todo', 'lists'], API.todo.list.getAll)
 }
 
 export const useTodoList = (id: string): UseQueryResult<TodoList, ApiError> => {
-  return useQuery(['todo', 'list', id], () => API().todo.list.getOne(id))
+  return useQuery(['todo', 'list', id], () => API.todo.list.getOne(id))
 }
