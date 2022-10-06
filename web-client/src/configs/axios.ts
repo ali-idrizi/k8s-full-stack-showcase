2096ebd64b102285ab86dfbb81b7e9a4c9833b59
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-07 01:47:56 +0200

    chore(web-client): remove baseUrl from main axios config

--- web-client/src/configs/axios.ts
+++ web-client/src/configs/axios.ts
@@ -1,10 +1,9 @@
 import { AxiosRequestConfig } from 'axios'
 
 export const axiosConfig: AxiosRequestConfig = {
-  baseURL: '/api',
   timeout: 5000,
   headers: {
-    'Content-Type': 'application/json',
     Accept: 'application/json',
+    'Content-Type': 'application/json',
   },
 }
