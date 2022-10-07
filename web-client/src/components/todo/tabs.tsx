9c1032239f9f80b2cb08f8e4b0936891aebf6a10
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-07 20:19:45 +0200

    chore(web-client): show actual lists in the todo tabs component

--- web-client/src/components/todo/tabs.tsx
+++ web-client/src/components/todo/tabs.tsx
@@ -1,11 +1,46 @@
 import { TooltipIconButton } from '@/components'
 import { useBrandColors } from '@/hooks'
-import { Tab, TabList, TabPanel, TabPanels, Tabs, useColorModeValue } from '@chakra-ui/react'
+import { useTodoLists } from '@/hooks/queries/todo-list'
+import {
+  Alert,
+  AlertIcon,
+  Spinner,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import Link from 'next/link'
+import { useMemo } from 'react'
 import { FiPlus } from 'react-icons/fi'
 
-export const TodoTabs: React.FC = () => {
+type Props = {
+  listId: string
+}
+
+export const TodoTabs: React.FC<Props> = ({ listId }) => {
+  const { data: todoLists, isLoading, isError } = useTodoLists()
   const { primaryScheme } = useBrandColors()
   const tabListBorderColor = useColorModeValue('gray.100', 'gray.700')
+
+  const index = useMemo(() => {
+    return todoLists?.findIndex((list) => list.id === listId) ?? 0
+  }, [todoLists, listId])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return (
+      <Alert status="error" rounded="md">
+        <AlertIcon />
+        An error occurred! Please try again later.
+      </Alert>
+    )
+  }
 
   return (
     <Tabs
@@ -14,7 +49,8 @@ export const TodoTabs: React.FC = () => {
       variant="solid-rounded"
       alignSelf="center"
       w="full"
-      maxW="96"
+      maxW="45rem"
+      index={index}
     >
       <TabList
         gap="2"
@@ -23,19 +59,19 @@ export const TodoTabs: React.FC = () => {
         borderBottom="2px solid"
         borderColor={tabListBorderColor}
       >
-        <Tab>One</Tab>
-        <Tab>Two</Tab>
+        {todoLists.map((list) => (
+          <Link key={list.id} href={`/dashboard/${list.id}`} passHref>
+            <Tab as="a">{list.title}</Tab>
+          </Link>
+        ))}
 
         <TooltipIconButton ml="auto" aria-label="Add New List" icon={<FiPlus />} />
       </TabList>
 
       <TabPanels>
-        <TabPanel>
-          <p>one!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
+        {todoLists.map((list) => (
+          <TabPanel key={list.id}>{list.title}</TabPanel>
+        ))}
       </TabPanels>
     </Tabs>
   )
--- web-client/src/pages/dashboard/[[...id]].tsx
+++ web-client/src/pages/dashboard/[[...id]].tsx
@@ -4,8 +4,10 @@ import { withApi } from '@/hocs/with-api'
 import { EmptyLayout } from '@/layouts'
 import { QUERY_KEY } from '@/utils/constants'
 import { PageWithLayout, TodoList } from '@/utils/types'
-import { Flex } from '@chakra-ui/react'
+import { Alert, AlertIcon, Flex } from '@chakra-ui/react'
 import { InferGetServerSidePropsType } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect, useRef } from 'react'
 
 export const getServerSideProps = withHocs(
   withReactQuery,
@@ -13,8 +15,6 @@ export const getServerSideProps = withHocs(
   withAuthenticatedRoute,
   withApi,
 )(async ({ queryClient, api }, ctx) => {
-  const paramListId = ctx.params?.id?.[0]
-
   let lists: TodoList[] = []
   try {
     lists = await queryClient.fetchQuery(
@@ -23,35 +23,53 @@ export const getServerSideProps = withHocs(
     )
   } catch (e) {}
 
-  const currentList =
-    lists.find((list) => list.id === paramListId) ?? lists.find((list) => list.default)
+  // Find the list based on URL parameter, if it doesn't exist, then find the default list.
+  // If there is no default list, then simply select the first list.
+  const paramListId = ctx.params?.id?.[0] ?? null
+  const activeList =
+    lists.find((list) => list.id === paramListId) ?? lists.find((list) => list.default) ?? lists[0]
 
-  if (currentList) {
-    if (paramListId !== currentList.id) {
-      return {
-        redirect: {
-          destination: `/dashboard/${currentList.id}`,
-          permanent: false,
-        },
-      }
-    }
-
-    await queryClient.prefetchQuery([QUERY_KEY.TODO, QUERY_KEY.TODO_LIST, currentList.id], () =>
-      api.todo.list.getOne(currentList.id),
+  if (activeList) {
+    await queryClient.prefetchQuery([QUERY_KEY.TODO, QUERY_KEY.TODO_LIST, activeList.id], () =>
+      api.todo.list.getOne(activeList.id),
     )
   }
 
   return {
-    props: {},
+    props: {
+      paramListId,
+      listId: activeList?.id ?? null,
+    },
   }
 })
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 
-const Dashboard: PageWithLayout<Props> = () => {
+const Dashboard: PageWithLayout<Props> = ({ paramListId, listId }) => {
+  const router = useRouter()
+  const routerRef = useRef(router)
+
+  useEffect(() => {
+    routerRef.current = router
+  }, [router])
+
+  useEffect(() => {
+    if (listId !== paramListId) {
+      routerRef.current.replace(`/dashboard/${listId}`, undefined, {
+        shallow: true,
+      })
+    }
+  }, [listId, paramListId])
+
   return (
     <Flex py={{ base: 8, md: 12 }} justifyContent="center">
-      <TodoTabs />
+      {listId ? (
+        <TodoTabs listId={listId} />
+      ) : (
+        <Alert status="error" rounded="md">
+          <AlertIcon />A server error occurred! Please contact us for assistance.
+        </Alert>
+      )}
     </Flex>
   )
 }
