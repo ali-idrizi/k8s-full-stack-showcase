9a2377b095df07e31f4f0919ad727b23e785695e
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-12 12:56:26 +0200

    chore(web-client): add todo item delete animation

--- web-client/src/components/todo/item/item.tsx
+++ web-client/src/components/todo/item/item.tsx
@@ -1,6 +1,6 @@
 import { useUpdateItemMutation } from '@/hooks/mutations'
 import { TodoItem } from '@/utils/types'
-import { Checkbox, Divider, ListItem } from '@chakra-ui/react'
+import { Checkbox, HStack } from '@chakra-ui/react'
 import { useRef } from 'react'
 import { DeleteTodoItem } from './delete'
 import { TodoTitle } from './title'
@@ -8,6 +8,7 @@ import { TodoTitle } from './title'
 type TodoListItemProps = {
   todo: TodoItem
 }
+
 export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
   const checkboxRef = useRef<HTMLInputElement>(null)
   const { mutate } = useUpdateItemMutation(todo.id)
@@ -19,20 +20,17 @@ export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
   }
 
   return (
-    <>
-      <ListItem data-group display="flex" alignItems="center" gap="2" py="3">
-        <Checkbox
-          ref={checkboxRef}
-          colorScheme="teal"
-          size="lg"
-          isChecked={todo.completed}
-          aria-label={todo.title}
-          onChange={onCheckboxChange}
-        />
-        <TodoTitle checkboxRef={checkboxRef} todo={todo} />
-        <DeleteTodoItem todo={todo} />
-      </ListItem>
-      <Divider orientation="horizontal" />
-    </>
+    <HStack data-group py="3">
+      <Checkbox
+        ref={checkboxRef}
+        colorScheme="teal"
+        size="lg"
+        isChecked={todo.completed}
+        aria-label={todo.title}
+        onChange={onCheckboxChange}
+      />
+      <TodoTitle checkboxRef={checkboxRef} todo={todo} />
+      <DeleteTodoItem todo={todo} />
+    </HStack>
   )
 }
--- web-client/src/components/todo/list/list.tsx
+++ web-client/src/components/todo/list/list.tsx
@@ -1,6 +1,7 @@
 import { ApiErrorAlert } from '@/components/common'
 import { useTodoList } from '@/hooks/queries/todo-list'
-import { Alert, AlertIcon, Center, List, Spinner } from '@chakra-ui/react'
+import { Alert, AlertIcon, Center, Divider, Fade, List, Spinner } from '@chakra-ui/react'
+import { AnimatePresence, motion } from 'framer-motion'
 import { TodoListItem } from '../item'
 import { TodoListHeader } from './header'
 
@@ -29,18 +30,31 @@ export const TodoList: React.FC<Props> = ({ listId }) => {
     <>
       <TodoListHeader list={list} />
 
-      {todoItems.length === 0 && (
-        <Alert status="info" rounded="md">
-          <AlertIcon />
-          You don't have any todos in this list! Use the field above to add some.
-        </Alert>
+      {todoItems.length === 0 ? (
+        <Fade in>
+          <Alert status="info" rounded="md">
+            <AlertIcon />
+            You don't have any todos in this list! Use the field above to add some.
+          </Alert>
+        </Fade>
+      ) : (
+        <List>
+          <AnimatePresence initial={false}>
+            {todoItems.map((item) => (
+              <motion.li
+                key={item.id}
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: 'auto', opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <TodoListItem todo={item} />
+                <Divider />
+              </motion.li>
+            ))}
+          </AnimatePresence>
+        </List>
       )}
-
-      <List>
-        {todoItems.map((item) => (
-          <TodoListItem key={item.id} todo={item} />
-        ))}
-      </List>
     </>
   )
 }
