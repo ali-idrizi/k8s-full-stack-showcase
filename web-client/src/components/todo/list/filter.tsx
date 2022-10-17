907cd1fcb66d0d01f66e4e451a4313c192c640ef
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-17 17:41:36 +0200

    refactor(web-client): improve import

--- web-client/src/components/todo/list/filter.tsx
+++ web-client/src/components/todo/list/filter.tsx
@@ -1,5 +1,4 @@
-import { useBrandColors } from '@/hooks'
-import { FilterItemsBy } from '@/hooks/queries/todo-list'
+import { FilterItemsBy, useBrandColors } from '@/hooks'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import React from 'react'
 
