470d55fa5620f32ce2753c252c7b136852bc8a8d
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-27 00:15:07 +0200

    chore(web-client): add link to github header button

--- web-client/src/components/common/tooltip-icon-button.tsx
+++ web-client/src/components/common/tooltip-icon-button.tsx
@@ -1,10 +1,9 @@
-import { IconButton, IconButtonProps } from '@chakra-ui/react'
+import { forwardRef, IconButton, IconButtonProps } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
-import { forwardRef } from 'react'
 
 const Tooltip = dynamic(() => import('@chakra-ui/tooltip').then((chakra) => chakra.Tooltip))
 
-export const TooltipIconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+export const TooltipIconButton = forwardRef<IconButtonProps, 'button'>(
   ({ 'aria-label': ariaLabel, ...rest }, ref) => {
     return (
       <Tooltip hasArrow label={ariaLabel} closeOnClick={false} mt="2">
