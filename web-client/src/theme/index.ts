1349153c611ea1c23471d19d670b738c413e2b5f
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-10-27 17:33:07 +0200

    refactor(web-client): refactor theme

--- web-client/src/theme/index.ts
+++ web-client/src/theme/index.ts
@@ -1,5 +1,6 @@
 import { extendTheme, ThemeConfig } from '@chakra-ui/react'
-import Button from './components/button'
+import { components } from './components'
+import { foundations } from './foundations'
 
 const config: ThemeConfig = {
   cssVarPrefix: 'ck',
@@ -7,23 +8,8 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 }
 
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const fontSizes = {
-  md: '1.05rem',
-}
-
 export default extendTheme({
-  colors,
+  ...foundations,
+  components,
   config,
-  components: {
-    Button,
-  },
-  fontSizes,
 })
