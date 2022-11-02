aba3c98b144038dae47bf8f2442a61cf62c56f5b
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-11-02 16:02:44 +0100

    chore(web-client): update container to use chakra's forwardRef

--- web-client/src/components/common/container.tsx
+++ web-client/src/components/common/container.tsx
@@ -1,12 +1,9 @@
-import { Container as ChakraContainer, ContainerProps, Flex } from '@chakra-ui/react'
+import { Container as ChakraContainer, ContainerProps, Flex, forwardRef } from '@chakra-ui/react'
 
-export const Container: React.FC<React.PropsWithChildren<ContainerProps>> = ({
-  children,
-  ...rest
-}) => {
+export const Container = forwardRef<ContainerProps, 'div'>(({ children, ...rest }, ref) => {
   return (
-    <ChakraContainer as={Flex} maxW="container.xl" {...rest}>
+    <ChakraContainer ref={ref} as={Flex} maxW="container.xl" {...rest}>
       {children}
     </ChakraContainer>
   )
-}
+})
