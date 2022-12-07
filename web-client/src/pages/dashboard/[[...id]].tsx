d42202a306f02da17c2998a724442c8f0b071d09
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2022-12-08 00:37:30 +0100

    refactor(web-client): remove unneeded state in dashboard page

--- web-client/src/pages/dashboard/[[...id]].tsx
+++ web-client/src/pages/dashboard/[[...id]].tsx
@@ -6,7 +6,6 @@ import { QUERY_KEY } from '@/utils/constants'
 import { PageWithLayout, TodoList } from '@/utils/types'
 import { Flex } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect, useState } from 'react'
 
 export const getServerSideProps = withHocs(
   withCookiesProp,
@@ -25,13 +24,14 @@ export const getServerSideProps = withHocs(
 
   // Find the list based on URL parameter, if it doesn't exist, then find the default list.
   // If there is no default list, then simply select the first list.
-  const paramListId = ctx.params?.id?.[0] ?? null
+  const paramListId = ctx.params?.id?.[0]
   const activeList =
     lists.find((list) => list.id === paramListId) ?? lists.find((list) => list.default) ?? lists[0]
-  const listId = activeList?.id ?? null
 
-  if (activeList) {
-    if (listId !== paramListId) {
+  const listId = activeList?.id
+
+  if (listId) {
+    if (paramListId !== listId) {
       return {
         redirect: {
           destination: `/dashboard/${listId}`,
@@ -40,8 +40,8 @@ export const getServerSideProps = withHocs(
       }
     }
 
-    await queryClient.prefetchQuery([QUERY_KEY.TODO, QUERY_KEY.TODO_LIST, activeList.id], () =>
-      api.todo.list.getOne(activeList.id),
+    await queryClient.prefetchQuery([QUERY_KEY.TODO, QUERY_KEY.TODO_LIST, listId], () =>
+      api.todo.list.getOne(listId),
     )
   }
 
@@ -52,12 +52,7 @@ export const getServerSideProps = withHocs(
 
 const Dashboard: PageWithLayout = () => {
   const router = useRouter()
-  const getListId = useCallback(() => router.query.id?.[0] ?? null, [router])
-  const [listId, setListId] = useState(() => getListId())
-
-  useEffect(() => {
-    setListId(getListId())
-  }, [getListId])
+  const listId = router.query.id?.[0] ?? null
 
   return (
     <Flex py={{ base: 8, md: 12 }} justifyContent="center">
